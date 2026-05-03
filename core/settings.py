c5087274e7343ee8dfb22abfbf08dc4fbb192424
shouni07 <maexueni@gmail.com>
2026-05-03 19:04:10 +0800

    fix os import

--- core/settings.py
+++ core/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/6.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,9 +23,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3rnt27t@ymcbs$t^mk*#v1$flfb@i@z@s&g*tjyorka$)33s!u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["localhost",
+"127.0.0.1",
+".onrender.com"]
 
 
 # Application definition
